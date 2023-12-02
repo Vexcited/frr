13d9d4b3d17fa0d2c134d5226229a60e166011e7
Mikkel RINGAUD <mikkel@milescode.dev>
2023-12-02 23:05:35 +0100

    chore: use an example for main program

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import { Lexer } from "./lexer";
 import { Parser } from "./ast";
 import { Interpreter } from "./interpreter";
 
-const text = "(((2)+2))/4*2";
+const text = `
+programme HelloWorld
+début
+  a <- 6
+  b <- b + 9
+fin HelloWorld
+`.trim();
 
 const lexer = new Lexer(text);
 const parser = new Parser(lexer);
-console.log(JSON.stringify(parser.parse(), null, 2));
-// const interpreter = new Interpreter(parser);
-// const result = interpreter.interpret();
-// console.log(result);
+const interpreter = new Interpreter(parser);
+const result = interpreter.interpret();
+console.log(result);
