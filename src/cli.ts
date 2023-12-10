82e0d7c426484812cac79a9a04bae07d3d688de2
Mikkel RINGAUD <mikkel@milescode.dev>
2023-12-10 18:57:42 +0100

    feat: write a minimal CLI

---
+++ src/cli.ts
@@ -0,0 +1,53 @@
+import fs from "node:fs";
+import { Lexer, Parser, SemanticAnalyzer, Interpreter } from ".";
+
+const USAGE_MESSAGE = "Utilisation: frr <destination/fichier/script.fr>";
+
+if (process.argv.length > 3) {
+  console.error("Seulement un paramètre peut être fourni.");
+  console.log(USAGE_MESSAGE);
+  process.exit(1);
+}
+
+const scriptPath = process.argv[2] as string | undefined;
+if (!scriptPath) {
+  console.log(USAGE_MESSAGE);
+  process.exit(0); // This is not an error.
+}
+
+// Check if the path is correct.
+if (!fs.existsSync(scriptPath)) {
+  console.error(`Le fichier "${scriptPath}" n'existe pas.`);
+  process.exit(2);
+}
+
+// Check if the given path is a file.
+const fileStats = fs.statSync(scriptPath);
+if (!fileStats.isFile()) {
+  console.error(`"${scriptPath}" n'est pas un fichier.`);
+  process.exit(3);
+}
+
+const code = await fs.promises.readFile(scriptPath, { encoding: "utf-8" });
+
+try {
+  const lexer = new Lexer(code);
+  const parser = new Parser(lexer);
+  const tree = parser.parse();
+
+  // We check the code for any syntax errors.
+  const symbol_table_builder = new SemanticAnalyzer();
+  symbol_table_builder.visit(tree);
+
+  // We interpret the code.
+  const interpreter = new Interpreter();
+  await interpreter.interpret(tree);
+}
+catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message);
+  }
+  else {
+    console.error("UnknownError:", error);
+  }
+}
