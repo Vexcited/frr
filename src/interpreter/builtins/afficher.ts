e94064f87562750759d6451ed6a4015b3a59b6f9
Mikkel RINGAUD <mikkel@milescode.dev>
2023-12-14 08:15:33 +0100

    feat(afficher): output reals with decimals on `.0`

--- src/interpreter/builtins/afficher.ts
+++ src/interpreter/builtins/afficher.ts
@@ -1,20 +1,40 @@
 import type { BuiltinProcedure } from ".";
+import { RealNumber, Variable } from "../../ast/nodes";
+
+/**
+ * Force to keep the .0 decimal part of a real number
+ * even if there's none.
+ *
+ * Example : 1.0 -> 1.0 (not 1)
+ */
+const readRealAsString = (value: number): string => {
+  const valueAsString = value.toString();
+  if (valueAsString.indexOf(".") === -1) {
+    return `${valueAsString}.0`;
+  }
+
+  return valueAsString;
+};
 
 export const afficher: BuiltinProcedure = {
   name: "afficher",
   call: async (scope, args) => {
-    const handleNodeValue = (value: unknown) => {
+    const handleNodeValue = (arg: typeof args[number]) => {
       // Boolean values are printed as "vrai" or "faux".
-      if (typeof value === "boolean") {
-        return value ? "vrai" : "faux";
+      if (typeof arg.value === "boolean") {
+        return arg.value ? "vrai" : "faux";
       }
 
-      return value;
+      if (arg.node instanceof Variable || arg.node instanceof RealNumber) {
+        return readRealAsString(arg.value as number);
+      }
+
+      return arg.value;
     };
 
     // Write to stdout instead of using `console.log` to avoid
     // - the newline at the end of the output
     // - format from "node:util"
-    process.stdout.write(args.map(arg => handleNodeValue(arg.value)).join(" "));
+    process.stdout.write(args.map(handleNodeValue).join(" "));
   }
 };
